d147d47ae17325b3516caefc0b019a9db208174f
edimez14 <129325241+edimez14@users.noreply.github.com>
2023-09-19 18:56:06 -0500

    add the file practica_juego_de_de_adivinanza_1.py
    I added the file practica_juego_de_1.py, which is the first version of the code where the game only has the function that two players guess a secret number.
    
    the secret number is generated randomly and the two players give their number randomly, depending on the number they give a message will be displayed on the console that will say if the player's number is higher or lower than the secret number, which will modify the search rate that the players have.

---
+++ practica_juego_de_adivinanza_1.py
@@ -0,0 +1,56 @@
+import os
+import random
+
+# vitorias_m1 = 0
+# vitorias_m2 = 0
+
+#ciclo de jugadas
+for i in range(1, 10):
+	inferior = 1
+	superior = 1000000
+	secreto = random.randint(1, 1000000)
+	maquina_1 = -1
+	maquina_2 = -1
+	print('<---   juego adivina el numero   --->')
+	while maquina_1 != secreto and maquina_2 != secreto:
+		
+		os.system('cls')
+		print('maquina_1, cual crees que es el numero?')
+		#jugador maquina_1
+		maquina_1 = random.randint(inferior, superior)
+		print(f'maquina_1: {maquina_1}')
+		if maquina_1 < secreto:
+			print('maquina tu numero es menor, intenta de nuevo')
+			inferior = maquina_1 + 1
+		elif maquina_1 > secreto:
+			print('maquina tu numero es mayor, intenta de nuevo')
+			superior = maquina_1 - 1
+		else:
+			print('has acertado el numero, la maquina_1 a ganado')
+		#jugador maquina_2
+		if maquina_1 != secreto:
+			maquina_2 = random.randint(inferior, superior)
+			print(f'maquina_2: {maquina_2}')
+			if maquina_2 < secreto:
+				print('maquina_2 tu numero es menor, intenta de nuevo')
+				inferior = maquina_2 + 1
+				if maquina_2 > inferior:
+						inferior = maquina_2 + 1
+			elif maquina_2 > secreto:
+				print('maquina_2 tu numero es mayor, intenta de nuevo')
+				superior = maquina_2 - 1
+				if maquina_2 < superior:
+					superior = maquina_2 - 1
+			else:
+				print('has acertado el numero, la maquina_2 a ganado')
+		# if maquina_1 == secreto:
+		# 	vitorias_m1+=1
+		# 	print(f'la maquina_1 a gando {vitorias_m1} veces')
+		# elif maquina_2 == secreto:
+		# 	vitorias_m2+=1
+		# 	print(f'la maquina_2 a gando {vitorias_m2} veces')
+
+
+	 	
+				
+	# input('preciona enter par acontinuar...')
