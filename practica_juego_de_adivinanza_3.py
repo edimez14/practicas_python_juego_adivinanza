0d6e6bdced1c05b8cc24d9febc6db081ba75a39a
edimez14 <129325241+edimez14@users.noreply.github.com>
2023-09-19 19:07:17 -0500

    i added the file practica_juego_de_3.py
    i added the file practica_juego_de_3.py, it is the third version of the code, this version has only two functionalities first is that two players guess a secret number, the second is that there is a win count. also the code is much more summarized.
    
    if a player guesses the secret number he wins and that win is added to his win counter.

---
+++ practica_juego_de_adivinanza_3.py
@@ -0,0 +1,72 @@
+"""
+este codigo tenia un error y es que no actualizaba los limites superior he inferior de la variable numero
+esto pasaba porque no se retornaba el valor de la variable numero, todo esto se soluciono retornando todos estos valores
+por mi inesperiencia no sabia muy bien como hayar la solucion y por eso pase por alto el retornar el valor de la vairble numeror
+por heyp tuve que pedirle ayuda a chatgpt para ver como encontrar una solucion a mi problema, asi que todo el credito a chatgpt
+"""
+
+# primero importamos los paquetes que vamos a usar
+import os #"os" es para limpiar la pantalla despues de cada ronda del ciclo
+import random #"random" es para generar de forma aleatoria un numero
+
+# Función para la lógica de adivinanza de números
+def adivinar_numero(secreto, inferior, superior, maquina):
+    numero = random.randint(inferior, superior)
+    print(f'{maquina}: {numero}')
+    if numero < secreto:
+        print(f'{maquina}, tu número es menor. Intenta de nuevo.\n')
+        inferior = numero + 1
+    elif numero > secreto:
+        print(f'{maquina}, tu número es mayor. Intenta de nuevo.\n')
+        superior = numero - 1
+    else:
+        print(f'¡{maquina} ha adivinado el número! ¡{maquina} ha ganado!\n')
+        return numero
+    return numero, inferior, superior
+
+# Inicializar el diccionario de victorias
+victorias = {'Maquina 1': 0, 'Maquina 2': 0}
+
+# Ciclo de jugadas
+for i in range(1, 10):
+    
+    inferior = 1
+    superior = 1000
+    secreto = random.randint(1, 1000)
+
+    # Variable para llevar el registro de qué máquina adivinó el número
+    ganador = None 
+
+    print('<---   juego adivina el numero   --->\n')
+
+    while ganador is None:
+
+        # os.system('cls')
+
+        print('Maquina 1, ¿cuál crees que es el número?')
+        resultado = adivinar_numero(secreto, inferior, superior, 'Maquina 1')
+        if isinstance(resultado, int):
+            ganador = 'Maquina 1'
+        else:
+            numero, inferior, superior = resultado
+
+        if ganador is None:
+            print('Maquina 2, ¿cuál crees que es el número?')
+            resultado = adivinar_numero(secreto, inferior, superior, 'Maquina 2')
+            if isinstance(resultado, int):
+                ganador = 'Maquina 2'
+            else:
+                numero, inferior, superior = resultado
+
+
+    # Actualizar el diccionario de victorias
+    victorias[ganador] += 1
+    # Imprimir el resultado fuera del ciclo while            
+    # print(f'¡{ganador} ha ganado!\n') 
+
+# Imprimir el diccionario de victorias de cada maquina despues de finalizar todas las rondas
+print(f"Maquina 1 ha ganado: {victorias['Maquina 1']} veces")
+print(f"Maquina 2 ha ganado: {victorias['Maquina 2']} veces")
+
+#input para cerrar la ventana y asi no se cierra de forma automatica
+input('Presione cualquier tecla para salir...')
